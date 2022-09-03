ac75495b4c83ae9e777d3a50b038e4e1eb42442f
JessRies <iammichaelariesyap@gmail.com>
2022-09-03 23:39:54 +0800

    messenger plugins

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,7 +19,6 @@ class Cors
         // header('Access-Control-Allow-Origin:  http://localhost:4200');
         // header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
         // header('Access-Control-Allow-Methods:  GET, POST, PUT, DELETE, OPTIONS');
-
         return $next($request)
         ->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', '*')
