6e8eb70ab92932efb181224052194b40e0ac1081
JessRies <iammichaelariesyap@gmail.com>
2022-09-05 21:09:36 +0800

    add subscriber function

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Subscribers;
-use App\Notifications\SubscriberNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Notifications\SubscriberNotification;
 
 class SubscribeController extends Controller
 {
@@ -16,13 +17,18 @@ class SubscribeController extends Controller
 
         $subscriber->save();
 
+        // $name = 'Cloudways';
+        
+
         $notifyData = [
             'body' => 'Thank you for Subscribing on our Website YOu will receive Email on Latest News & Announcements',
             'text' => 'Visit Our Website',
             'url'   => url('/'),
             'footer' => 'Thank you for using our application!'
         ];
-        $subscriber->notify(new SubscriberNotification($notifyData));
+        Mail::to('Cloudways@Cloudways.com')->send(new SubscriberNotification($notifyData));
+
+        // $subscriber->notify(new SubscriberNotification($notifyData));
         
         return redirect()->back()->with('message','Thank You for Subscribing to our website');
     }
