53381c1b1a99827e3a88605725bb7b7b44e24f9f
JessRies <iammichaelariesyap@gmail.com>
2022-09-14 09:47:15 +0800

    Updated

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -24,47 +24,67 @@
 <div class="container mb-5 mt-4" >
     <div class="col-12">
         <div class="card border-0">
-            <div class="card-body">
-                <!-- Event 1 -->
-                <div class="row ">
-                    @foreach ($allEvents as $item)
-                        <div class="col-12 row align-items-center d-block d-sm-flex ">
-                            <div class="col-auto mb-3 mb-sm-0">
-                                <div class="calendar d-flex">
-                                    <span class="calendar-month  bg-primary ">{{ date('M ', strtotime($item->start)) }}</span>
-                                    <span class="calendar-day py-2">{{ date('d', strtotime($item->start)) }}</span>
+            @if ($allEvents->count())
+                
+                <div class="card-body">
+                    <!-- Event 1 -->
+                    <div class="row ">
+                        @foreach ($allEvents as $item)
+                            <div class="col-12 row align-items-center d-block d-sm-flex ">
+                                <div class="col-auto mb-3 mb-sm-0">
+                                    <div class="calendar d-flex">
+                                        <span class="calendar-month  bg-primary ">{{ date('M ', strtotime($item->start)) }}</span>
+                                        <span class="calendar-day py-2">{{ date('d', strtotime($item->start)) }}</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col">
-                                <a href="{{ url('university_events/'.$item->title ) }}">
-                                    <h3 class="h4 mb-1 _w-icon">{{ $item->title }} 
-                                </h3>
+                                <div class="col">
+                                    <a href="{{ url('university_events/'.$item->title ) }}">
+                                        <h3 class="h4 mb-1 _w-icon">{{ $item->title }} 
+                                    </h3>
+                                        
+                                    </a>
+                                    <span>
+                                        Organized by 
+                                        <a href="#">{{ $item->organizer }}</a>
+                                    </span>
                                     
-                                </a>
-                                <span>
-                                    Organized by 
-                                    <a href="#">{{ $item->organizer }}</a>
-                                </span>
-                                
-                                <div class="small fw-bold">
-                                    <span class="text-danger">Date</span>:
-                                    
-                                    {{ date('M d,Y', strtotime($item->start)) }} - {{ date('M d,Y', strtotime($item->end)) }}
+                                    <div class="small fw-bold">
+                                        <span class="text-danger">Date</span>:
+                                        
+                                        {{ date('M d,Y', strtotime($item->start)) }} - {{ date('M d,Y', strtotime($item->end)) }}
+                                    </div>
+                                    <span class="small fw-bold">
+                                        <span class="text-danger">Place</span>: {{ $item->location }}
+                                    </span>
+                                    <p class="lead fs-6 mt-1">
+                                        {!! Str::limit($item->description, 200) !!}
+                                    </p>
                                 </div>
-                                <span class="small fw-bold">
-                                    <span class="text-danger">Place</span>: {{ $item->location }}
-                                </span>
-                                <p class="lead fs-6 mt-1">
-                                    {!! Str::limit($item->description, 200) !!}
-                                </p>
+                                <hr>
                             </div>
-                            <hr>
-                        </div>
-                    @endforeach
+                        @endforeach
 
-                    {{ $allEvents->links() }}
+                        {{ $allEvents->links() }}
+                    </div>
                 </div>
-            </div>
+            @else
+                <div class="bg-primary card-body text-white mb-5 rounded-md">
+                    <div class="row">
+                        <div class="col-2">
+                            <img class="w-100" src="https://cdn.shopify.com/s/files/1/1061/1924/products/Sad_Face_Emoji_large.png?v=1571606037" alt="">
+                        </div>
+                        <div class="col-10">
+                            <h4 class="text-white">SORRY !</h4>
+                            <p class="lead">
+                                NO POST AVAILABLE RIGHT NOW 
+                                <br>
+                                PLEASE COMEBACK LATER THANK YOU..
+                            </p>
+                            
+                        </div>
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 </div>
