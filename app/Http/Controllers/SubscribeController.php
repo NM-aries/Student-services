1d0128b1c5f89cc5b9d18bdf8afca265020c5be5
NM-aries <iammichaelariesyap@gmail.com>
2022-10-16 14:00:41 +0800

    update

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -28,7 +28,6 @@ class SubscribeController extends Controller
         $subscriber = new Subscribers();
         $subscriber->email = $request->input('email');
         $subscriber->name = $request->input('name');
-        // $subscriber->contact_number = $request->input('contact_number');
         $subscriber->save();
 
         $subscriber->notify(new SubscriberNotification($subscriber));
@@ -51,34 +50,5 @@ class SubscribeController extends Controller
         session()->flash('message', 'Email Successfully Sent');
         return redirect()->back();
     }
-
-    // public function sms()
-    // {
-    //     $subs = Subscribers::all();
-    //     return view('admin._subscribers.sms' , compact('subs'));
-    // }
-
-    // public function sendSms(Request $request)
-    // {
-    
-    //     try{
-    //         $sid    = "AC9b189ce47f71ad16432f7f0bcc496d23"; 
-    //         $token  = "21be333c43a5bc512dd0f857f6e97acc";
-    //         $number = "+19378723140";
-    //         $twilio = new Client($sid, $token); 
-         
-    //     $twilio->messages->create('+63'.$request->contact_number, [
-    //                 'from' => $number,
-    //                 'body' => $request->message
-    //             ]); 
-
-    //             return 'message sent';
-    //     }catch(\Exception $e)
-    //     {
-    //         return   $e->getMessage();
-    //     }
-        
-         
-        // print($message->sid);        
 
 }
--- database/migrations/2022_09_04_125737_create_subscribers_table.php
+++ database/migrations/2022_09_04_125737_create_subscribers_table.php
@@ -17,7 +17,6 @@ class CreateSubscribersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email');
-            $table->string('contact_number')->nullable;
             $table->timestamps();
         });
     }
